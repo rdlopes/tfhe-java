cd81642266d80d27f4528d09a0b8cc8043dbc172
Rui Lopes <rui.d.lopes@me.com>
2025-08-30 21:11:00 +0200

    feat: remove JCA implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,7 @@ module tfhe.core {
   exports io.github.rdlopes.tfhe.core.configuration;
   exports io.github.rdlopes.tfhe.core.keys;
   exports io.github.rdlopes.tfhe.core.serde;
-  exports io.github.rdlopes.tfhe.jca;
   requires org.apache.commons.io;
   requires org.apache.commons.lang3;
   requires org.slf4j;
-  provides java.security.Provider with io.github.rdlopes.tfhe.jca.TfheProvider;
 }
